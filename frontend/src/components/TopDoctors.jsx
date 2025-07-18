0510d279ecf7397c34079fd2e44d8edb2355e846
Varun-3131 <srivastavvarun48@gmail.com>
2025-07-18 15:44:57 +0530

    Related Doctors

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -15,7 +15,7 @@ const TopDoctors = () => {
 
             <div className="w-full grid [grid-template-columns:repeat(auto-fill,minmax(200px,1fr))] gap-4 pt-5 gap-y-6 px-3 sm:px-0">
                 {doctors.slice(0, 10).map((item, index) => (
-                    <div key={item._id} onClick={()=>navigate(`/appointment/${item._id}`)} className="border border-blue-300 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
+                    <div key={item._id} onClick={()=> {navigate(`/appointment/${item._id}`);scrollTo(0,0)}} className="border border-blue-300 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500">
                         <img className="bg-blue-50" src={item.image} alt=""/>
                         <div className="p-4">
                             <div className="flex items-center gap-2 text-sm text-center text-green-500">
