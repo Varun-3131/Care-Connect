c79f3daa0fa1b5616ed97629e55cef7ca4f20190
Varun-3131 <srivastavvarun48@gmail.com>
2025-07-19 19:06:59 +0530

    Backend

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+
+const app=express()
+const port=process.env.PORT || 4000
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("PORT LISTENING ")
+})
+
+app.listen(port,()=>{
+    console.log(`Server started on port ${port}`)
+})
